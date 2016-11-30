9f5d1bc5d32109f8dd373f45280d9b3c5561313f
Tal Kot <talkot123@gmail.com>
2016-11-30 11:28:28 +0200

    Schedule GUI bug fix

---
+++ src/com/finalproject/Course.java
@@ -0,0 +1,70 @@
+package com.finalproject;
+
+public class Course 
+{
+	private int CourseNumber;
+	private String Name;
+	private String Semester;
+	private int HourseAmount;
+	private int Year;
+	private int Day;
+	private int Time_Hour;
+	private int Time_Minute;
+	
+	public int getCourseNumber() {
+		return CourseNumber;
+	}
+	public void setCourseNumber(int courseNumber) {
+		CourseNumber = courseNumber;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getSemester() {
+		return Semester;
+	}
+	public void setSemester(String semester) {
+		Semester = semester;
+	}
+	public int getHourseAmount() {
+		return HourseAmount;
+	}
+	public void setHourseAmount(int hourseAmount) {
+		HourseAmount = hourseAmount;
+	}
+	public int getYear() {
+		return Year;
+	}
+	public void setYear(int year) {
+		Year = year;
+	}
+	public int getDay() {
+		return Day;
+	}
+	public void setDay(int day) {
+		Day = day;
+	}
+	public int getTime_Hour() {
+		return Time_Hour;
+	}
+	public void setTime_Hour(int time_Hour) {
+		Time_Hour = time_Hour;
+	}
+	public int getTime_Minute() {
+		return Time_Minute;
+	}
+	public void setTime_Minute(int time_Minute) {
+		Time_Minute = time_Minute;
+	}
+	@Override
+	public String toString() {
+		return "Number=" + CourseNumber + ", Name=" + Name + ", Semester=" + Semester + ", HourseAmount="
+				+ HourseAmount + ", Year=" + Year + ", Day=" + Day + ", Time_Hour=" + Time_Hour + ", Time_Minute="
+				+ Time_Minute + "]";
+	}
+	
+	
+}
---
+++ src/com/finalproject/Lecture.java
@@ -0,0 +1,75 @@
+package com.finalproject;
+
+public class Lecture {
+	private int ID;
+	private String FirstName;
+	private String LastName;
+	private int BirthdayDay;
+	private int BirthdayMonth;
+	private int BirthdayYear;
+	private String AdressCity;
+	private int AdressStreetNumber;
+	private String AdressName;
+
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public int getBirthdayDay() {
+		return BirthdayDay;
+	}
+	public void setBirthdayDay(int birthdayDay) {
+		BirthdayDay = birthdayDay;
+	}
+	public int getBirthdayMonth() {
+		return BirthdayMonth;
+	}
+	public void setBirthdayMonth(int birthdayMonth) {
+		BirthdayMonth = birthdayMonth;
+	}
+	public int getBirthdayYear() {
+		return BirthdayYear;
+	}
+	public void setBirthdayYear(int birthdayYear) {
+		BirthdayYear = birthdayYear;
+	}
+	public String getAdressCity() {
+		return AdressCity;
+	}
+	public void setAdressCity(String adressCity) {
+		AdressCity = adressCity;
+	}
+	public int getAdressStreetNumber() {
+		return AdressStreetNumber;
+	}
+	public void setAdressStreetNumber(int i) {
+		AdressStreetNumber = i;
+	}
+	public String getAdressName() {
+		return AdressName;
+	}
+	public void setAdressName(String adressName) {
+		AdressName = adressName;
+	}
+	@Override
+	public String toString() {
+		return "ID=" + ID + ", FirstName=" + FirstName + ", LastName=" + LastName + ", BirthdayDay="
+				+ BirthdayDay + ", BirthdayMonth=" + BirthdayMonth + ", BirthdayYear=" + BirthdayYear + ", AdressCity="
+				+ AdressCity + ", AdressStreetNumber=" + AdressStreetNumber + ", AdressName=" + AdressName;
+	}
+
+}
